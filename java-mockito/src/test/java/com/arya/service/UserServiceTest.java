eb669d36de0d3e74de42c0d060d2e6797ea0234d
Prince Arya <prince422005@gmail.com>
2026-01-13 23:17:42 +0530

    mockito anotations learn

--- java-mockito/src/test/java/com/arya/service/UserServiceTest.java
+++ java-mockito/src/test/java/com/arya/service/UserServiceTest.java
@@ -1,6 +1,8 @@
 package com.arya.service;
 
+import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.InjectMocks;
 import org.mockito.Mock;
 import org.mockito.junit.jupiter.MockitoExtension;
 
@@ -9,7 +11,17 @@ import com.arya.repository.UserRepository;
 @ExtendWith(MockitoExtension.class)
 public class UserServiceTest {
 
-    //mock the userRepo, fake no logic in
+    //mock the userRepo, fake no logic inside(like a stunt double)
     @Mock
     private UserRepository userRepository;
+
+    //@InjectMocks , it'll create real obj and insert mocks into it from available pool, it searches... first constructor inject then field if fails.. uses reflection etc
+    //equiv: userService = new UserService(userRepo)
+    @InjectMocks
+    private UserService userService;
+
+    @Test
+    void testRegisterUser_Success(){
+        
+    }
 }
