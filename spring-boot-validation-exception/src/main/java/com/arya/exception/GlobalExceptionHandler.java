3b3f0e9ce2c1eb59f3b67194ec3270ac30465085
Prince Arya <prince422005@gmail.com>
2026-02-02 19:08:00 +0530

    exception trial

--- spring-boot-validation-exception/src/main/java/com/arya/exception/GlobalExceptionHandler.java
+++ spring-boot-validation-exception/src/main/java/com/arya/exception/GlobalExceptionHandler.java
@@ -1,5 +1,8 @@
 package com.arya.exception;
 
+import java.util.HashMap;
+import java.util.Map;
+
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ProblemDetail;
 import org.springframework.web.bind.MethodArgumentNotValidException;
@@ -12,10 +15,31 @@ import org.springframework.web.bind.annotation.RestControllerAdvice;
 public class GlobalExceptionHandler {
 
     //handle validation errors
-    // @ExceptionHandler
-    // public ProblemDetail handleValidationErrors (MethodArgumentNotValidException ex){
-    //     ProblemDetail prob = ProblemDetail.forStatus(HttpStatus.BAD_REQUEST);
-    //     prob.setTitle("Validation failed");
+    @ExceptionHandler(MethodArgumentNotValidException.class)
+    public ProblemDetail handleValidationErrors (MethodArgumentNotValidException ex){
+        ProblemDetail prob = ProblemDetail.forStatus(HttpStatus.BAD_REQUEST);
+        prob.setTitle("Validation failed");
         
-    // }
+        //like a response container, key = field name
+        //value = validation msg
+        //binding is like request -> obj
+        //getBindingResult is for getting a container from spring having some.. failures
+        Map<String, String> errors = new HashMap<>();
+        ex.getBindingResult()
+            .getFieldErrors()
+            .forEach(error -> {
+                errors.put(error.getField(), error.getDefaultMessage());
+            });
+
+        prob.setProperty("errors", errors); //custom field
+        return prob;
+    }
+
+    //handle businees logic errors, like id not found
+    public ProblemDetail handleEmployeeNotFound(EmployeeNotFoundException ex){
+        ProblemDetail prob = ProblemDetail.forStatus(HttpStatus.NOT_FOUND);
+        prob.setTitle("Resource not found...");
+        prob.setDetail(ex.getMessage());
+        return prob;
+    }
 }
