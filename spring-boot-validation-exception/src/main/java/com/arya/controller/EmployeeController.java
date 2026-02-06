bdb140bd036391b69e0fd3e57c1c49046acd2865
Prince Arya <prince422005@gmail.com>
2026-02-06 15:43:18 +0530

    more on service and controller

--- spring-boot-validation-exception/src/main/java/com/arya/controller/EmployeeController.java
+++ spring-boot-validation-exception/src/main/java/com/arya/controller/EmployeeController.java
@@ -1,5 +1,6 @@
 package com.arya.controller;
 
+import org.springframework.data.domain.Page;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -8,11 +9,14 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.arya.dto.EmployeeRequestDto;
+import com.arya.entity.Employee;
 import com.arya.service.EmployeeService;
 
 import jakarta.validation.Valid;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestParam;
+
 
 
 @RestController //means controller as well as Response body , i.e., response will be in json/xml dirrectly not views
@@ -33,14 +37,25 @@ public class EmployeeController {
     //constraints dont execute themselves, @valid will
     //req. body is for json req and uses httpmessageconverter, modelAttribute for form
     public ResponseEntity<String> create(@RequestBody @Valid EmployeeRequestDto dto){
-        String id = service.createEmployee(dto);
+        Long id = service.createEmployee(dto);
         return new ResponseEntity<>("created id: "+id, HttpStatus.CREATED);
     }
 
     @GetMapping("/{id}")
-    public ResponseEntity<EmployeeRequestDto> get(@PathVariable String id) {
+    public ResponseEntity<EmployeeRequestDto> get(@PathVariable Long id) {
         return ResponseEntity.ok(service.getEmployee(id));
         // ResponseEntity<>(service.getEmployee(id), HttpStatus.OK);
     }
+
+    @GetMapping
+    public ResponseEntity<Page<Employee>> getAll(
+        @RequestParam(defaultValue = "0") int page,
+        @RequestParam(defaultValue = "10") int size,
+        @RequestParam(defaultValue = "id") String sortBy,
+        @RequestParam(required = false) String department
+    ) {
+        return ResponseEntity.ok(service.getEmployees(page, size, sortBy, department));
+    }
+    
     
 }
--- spring-boot-validation-exception/src/main/java/com/arya/service/EmployeeService.java
+++ spring-boot-validation-exception/src/main/java/com/arya/service/EmployeeService.java
@@ -1,29 +1,71 @@
 package com.arya.service;
 
 import java.util.HashMap;
+import java.util.Optional;
 import java.util.UUID;
 
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Pageable;
 import org.springframework.stereotype.Service;
 
 import com.arya.dto.EmployeeRequestDto;
+import com.arya.entity.Employee;
+import com.arya.exception.EmployeeNotFoundException;
+import com.arya.repo.EmployeeRepository;
 
 //controller just routes things, service handle business logic
 @Service
 public class EmployeeService {
 
-    private final HashMap<String, EmployeeRequestDto> dummyDb = new HashMap<>();
+    // private final HashMap<String, EmployeeRequestDto> dummyDb = new HashMap<>();
 
-    public String createEmployee(EmployeeRequestDto dto){
-        //in production, here i'll convert dto->entity(db)
-        String id = UUID.randomUUID().toString();
-        dummyDb.put(id, dto);
-        return id;
+    // public String createEmployee(EmployeeRequestDto dto){
+    //     //in production, here i'll convert dto->entity(db)
+    //     String id = UUID.randomUUID().toString();
+    //     dummyDb.put(id, dto);
+    //     return id;
+    // }
+
+    // public EmployeeRequestDto getEmployee(String id){
+    //     if(!dummyDb.containsKey(id)){
+    //         // throw new EmployeeNotFound
+    //     }
+    //     return dummyDb.get(id);
+    // }
+
+    private final EmployeeRepository repository;
+
+    public EmployeeService(EmployeeRepository repository){
+        this.repository = repository;
     }
 
-    public EmployeeRequestDto getEmployee(String id){
-        if(!dummyDb.containsKey(id)){
-            // throw new EmployeeNotFound
+    public Long createEmployee(EmployeeRequestDto dto){
+        //map dto -> entity
+        Employee employee = new Employee(dto.name(), dto.email(), dto.departrment());
+        return repository.save(employee).getId();
+    }
+
+    //pagination logic
+    public Page<Employee> getEmployees(int page, int size, String sortBy, String departmentFilter){
+        //create Pageable object (Page numbers start at 0)
+        Pageable pageable = PageRequest.of(page, size);
+
+        //apply filtering if exist
+        if(departmentFilter != null && !departmentFilter.isEmpty()){
+            return repository.findByDepartment(departmentFilter, pageable);
         }
-        return dummyDb.get(id);
+
+        //return generic page
+        return repository.findAll(pageable);
+    }
+
+    public EmployeeRequestDto getEmployee(Long id){
+        Optional<Employee> e = repository.findById(id);
+        if(!e.isPresent()){
+            return new EmployeeRequestDto("na", "", "");
+        }
+        Employee emp = e.get();
+        return new EmployeeRequestDto(emp.getName(), emp.getEmail(), emp.getDepartment());
     }
 }
